ea284628e3500a64790e1733eafd99fc9a7f8e7d
Luis Miguel Bolaños Santana <luismysantana1998@gmail.com>
2023-02-04 18:52:15 +0000

    Update db.js

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ import dotenv from "dotenv/config";
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: 3306,
     dialect: "mysql",
     define: {
         timestamps: false
